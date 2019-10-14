c05ef1ccd10a36fa2ba0a04f9f66a016c7acb73a
sigit <sigit.setiaji@vizitrip.com>
2019-10-14 10:01:37 +0700

    add link url hacktoberfest

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Linking } from 'react-native';
+
+const urlHacktoberFest = "https://hacktoberfest.digitalocean.com"
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Welcome to Hacktoberfest 2019!</Text>
+        <Text onPress={() => Linking.openURL(urlHacktoberFest)}>
+          {urlHacktoberFest}
+        </Text>
       </View>
     );
   }
